76df85bb1e30a25ef2a84ef735765548a900c5d3
Tobias Kirsch <tkirsch@em.uni-franfurt.de>
2026-04-22 10:30:06 +0200

    adsf

--- run.py
+++ run.py
@@ -10,7 +10,6 @@ import networkx as nx
 import pandas as pd 
 import numpy as np
 import torch
-import multiprocessing as mp
 import json
 
 from matrix_bgpsim import RMatrix
@@ -56,78 +55,14 @@ def calculate_impact(
     return len(directly_affected) + len(indirectly_affected), len(directly_affected), len(indirectly_affected)
 
 
-def worker_init(gpu_queue, analysis_graph, stripped_graph):
-    global GPU_ID, WORKER_BASE_R_MATRIX, WORKER_ANALYSIS_GRAPH, WORKER_STRIPPED_GRAPH
-    GPU_ID = gpu_queue.get()
-    WORKER_ANALYSIS_GRAPH = analysis_graph
-    WORKER_STRIPPED_GRAPH = stripped_graph
-    
-    WORKER_BASE_R_MATRIX = RMatrix(
-        input_rels=os.path.join(ROOT_DIR, "network-graph-data", "as-rel.txt"),
-        excluded=set(analysis_graph.nodes()) - set(stripped_graph.nodes())
-    )
-    WORKER_BASE_R_MATRIX.run(
-        max_iter=32,
-        save_next_hop=True,
-        backend="torch",
-        device=f"cuda:{GPU_ID}"
-    )
-
-
-def worker_task(deployment_info):
-    method, adoption_rate, dropout, attacks = deployment_info
-    
-    pkl_path = os.path.join(ROOT_DIR, "deployments", f"{method.__name__}_{adoption_rate}_{dropout}.pkl")
-    with open(pkl_path, "rb") as f:
-        deployment_nodes = pickle.load(f)
-
-    component_graph = WORKER_ANALYSIS_GRAPH.copy()
-    component_graph.remove_nodes_from(deployment_nodes)
-
-    components = list(nx.connected_components(component_graph))
-    component_lengths = list(map(len, components))
-
-    deployment_r_matrix = RMatrix(
-        input_rels=os.path.join(ROOT_DIR, "network-graph-data", "as-rel.txt"), 
-        excluded=set(WORKER_ANALYSIS_GRAPH.nodes()) - set(WORKER_STRIPPED_GRAPH.nodes()) - set(deployment_nodes)
-    )
-    deployment_r_matrix.run(
-        max_iter=32,        
-        save_next_hop=True, 
-        backend="gpu",    
-        device=f"cuda:{GPU_ID}"
-    )
-    
-    num_nodes = len(WORKER_STRIPPED_GRAPH.nodes)
-    
-    for attacker, victim, _, real in attacks:
-        impact, direct_impact, indirect_impact = calculate_impact(
-            WORKER_STRIPPED_GRAPH,
-            WORKER_BASE_R_MATRIX,
-            deployment_r_matrix,
-            attacker,
-            victim
-        )
-
-        json.dump({
-            "adoption_rate": adoption_rate,
-            "dropout": dropout,
-            "impact": impact / num_nodes,
-            "direct_impact": direct_impact / num_nodes,
-            "indirect_impact": indirect_impact / num_nodes,
-            "method": method.__name__,
-            "attacker": attacker,
-            "victim": victim,
-            "number_of_components": len(components),
-            "max_component": max(component_lengths),
-            "average_component": np.mean(component_lengths),
-            "mode": "real_hijack" if real else "fake_hijack"
-        }, open(os.path.join(ROOT_DIR, "results", f"{method.__name__}_{adoption_rate}_{dropout}_{attacker}_{victim}.json"), "w"))
-
-
-def compute_impact(rel_file: str = None):
+def compute_impact(
+    methods: list[str],
+    rel_file: str = None,
+    device: str = "cuda:0",
+):
     if rel_file is None:
         rel_file = os.path.join(ROOT_DIR, "network-graph-data", "as-rel.txt")
+
     analysis_graph = create_graph(directed=False, edge_file=rel_file)
     stripped_graph = analysis_graph.copy()
     stripped_graph.remove_nodes_from([node for node in stripped_graph.nodes if stripped_graph.nodes[node]["type"] == "edge"])
@@ -137,47 +72,94 @@ def compute_impact(rel_file: str = None):
     
     compute_graph_metadata(stripped_graph)
     
-    deployments = get_deployments(stripped_graph, directed_graph)
+    deployments = get_deployments(methods, stripped_graph, directed_graph)
     attacks = get_attacks()
-    
-    num_gpus = torch.cuda.device_count() if torch.cuda.is_available() else 0
-    if num_gpus == 0:
-        num_gpus = 1 # Fallback to 1 if no GPUs detected, but backend is still specified as 'gpu'
         
-    ctx = mp.get_context('spawn')
-    m = ctx.Manager()
-    gpu_queue = m.Queue()
-    for i in range(num_gpus):
-        gpu_queue.put(i)
-        
-    tasks = []
-    for deployment in deployments:
-        method, adoption_rate, dropout = deployment
-        tasks.append((method, adoption_rate, dropout, attacks))
-        
+    base_r_matrix = RMatrix(
+        input_rels=os.path.join(ROOT_DIR, "network-graph-data", "as-rel.txt"),
+        excluded=set(analysis_graph.nodes()) - set(stripped_graph.nodes())
+    )
+    base_r_matrix.run(
+        max_iter=32,
+        save_next_hop=True,
+        backend="torch",
+        device=device
+    )
+    base_r_matrix.dump("results/base_r_matrix.lz4")
+
     all_results = []
     
-    with ctx.Pool(
-        processes=num_gpus, 
-        initializer=worker_init, 
-        initargs=(gpu_queue, analysis_graph, stripped_graph)
-    ) as pool:
-        for res_list in pool.map(worker_task, tasks):
-            all_results.extend(res_list)
+    for deployment in deployments:
+        method, adoption_rate, dropout = deployment
+        
+        pkl_path = os.path.join(ROOT_DIR, "deployments", f"{method.__name__}_{adoption_rate}_{dropout}.pkl")
+        with open(pkl_path, "rb") as f:
+            deployment_nodes = pickle.load(f)
+
+        component_graph = analysis_graph.copy()
+        component_graph.remove_nodes_from(deployment_nodes)
+
+        components = list(nx.connected_components(component_graph))
+        component_lengths = list(map(len, components))
+
+        deployment_r_matrix = RMatrix(
+            input_rels=os.path.join(ROOT_DIR, "network-graph-data", "as-rel.txt"), 
+            excluded=set(analysis_graph.nodes()) - set(stripped_graph.nodes()) - set(deployment_nodes)
+        )
+        deployment_r_matrix.run(
+            max_iter=32,        
+            save_next_hop=True, 
+            backend="torch",    
+            device=device
+        )
+        deployment_r_matrix.dump("results/{method.__name__}_{adoption_rate}_{dropout}.lz4")
+
+        num_nodes = len(stripped_graph.nodes)
+        
+        for attacker, victim, _, real in attacks:
+            impact, direct_impact, indirect_impact = calculate_impact(
+                stripped_graph,
+                base_r_matrix,
+                deployment_r_matrix,
+                attacker,
+                victim
+            )
+
+            res = {
+                "adoption_rate": adoption_rate,
+                "dropout": dropout,
+                "impact": impact / num_nodes,
+                "direct_impact": direct_impact / num_nodes,
+                "indirect_impact": indirect_impact / num_nodes,
+                "method": method.__name__,
+                "attacker": attacker,
+                "victim": victim,
+                "number_of_components": len(components),
+                "max_component": max(component_lengths),
+                "average_component": np.mean(component_lengths),
+                "mode": "real_hijack" if real else "fake_hijack"
+            }
+            json.dump(res, open(os.path.join(ROOT_DIR, "results", f"{method.__name__}_{adoption_rate}_{dropout}_{attacker}_{victim}.json"), "w"))
+            all_results.append(res)
             
     return pd.DataFrame(all_results)
 
 
-def get_deployments(analysis_graph, directed_graph):
+def get_deployments(methods: list[str], analysis_graph, directed_graph):
     TOP_100 = top_100(directed_graph, None)
 
-    methods = [
+    possible_methods = [
         random_choice, 
         cone_size,
         degree_centrality, 
         louvain_communities,
         kernighan_lin_partition
     ]
+    methods = []
+
+    for method in possible_methods:
+        if method.__name__ in methods:
+            methods.append(method)
 
     adoption_rates = [
         0.05,
