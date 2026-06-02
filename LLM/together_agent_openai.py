8b0f8d72466db0858b6f65331b9e2568d4b3e394
Dr. Shujie Zhao <104205668+zsjstart@users.noreply.github.com>
2026-06-02 11:20:05 +0200

    Update together_agent_openai.py

--- LLM/together_agent_openai.py
+++ LLM/together_agent_openai.py
@@ -19,7 +19,7 @@ import as_relationship
 
 
 
-TOGETHER_API_KEY = "tgp_v1_8Y3DAJ-NGohHEvtH2rgCu6j5rSeGU6-Da84EQs6BKtM"
+TOGETHER_API_KEY = "#####"
 TOGETHER_URL = "https://api.together.xyz/v1/chat/completions"
 
 
